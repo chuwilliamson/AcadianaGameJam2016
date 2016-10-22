f647974dc5db23a29ad686a4cf2a850ed347b19d
chuwilliamson <chuwilliamson@gmail.com>
2016-10-21 22:22:15 -0500

    build a hashtagbobby

--- ArmAndLeg/Assets/Scripts/EnemyController.cs
+++ ArmAndLeg/Assets/Scripts/EnemyController.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Items;
+public class EnemyController : MonoBehaviour
+{
 
-public class EnemyController : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+        GameObject armLeftobj = BuildLimb(armPrefab, new Arm(), true);
+        GameObject armRightobj = BuildLimb(armPrefab, new Arm());
+        GameObject legRightobj = BuildLimb(legPrefab, new Leg(), true);
+        GameObject legLeftobj = BuildLimb(legPrefab, new Leg());
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    GameObject BuildLimb(GameObject template, Limb limb, bool flipX = false, bool flipY = false)
+    {
+        GameObject obj = Instantiate(template, transform) as GameObject;
+        LimbBehaviour objComp = obj.AddComponent<LimbBehaviour>();
+        objComp.Init(limb);
+        obj.GetComponent<SpriteRenderer>().flipX = flipX;
+        obj.GetComponent<SpriteRenderer>().flipY = flipY;
+        m_Inventory.AddLimb(limb);
+
+        return obj;
+    }
+
+    // Update is called once per frame
+    public Inventory m_Inventory;
+    public GameObject armPrefab;
+    public GameObject legPrefab;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {        
+        m_Inventory.PopArm(transform);
+    }
 }
--- ArmAndLeg/Assets/Scripts/ZombiePlayerController.cs
+++ ArmAndLeg/Assets/Scripts/ZombiePlayerController.cs
@@ -4,8 +4,6 @@ using Items;
 
 public class ZombiePlayerController : MonoBehaviour
 {
-    public GameObject armPrefab;
-    public GameObject legPrefab;
     public Animator m_anim;
     // Use this for initialization
     void Awake()
@@ -14,20 +12,10 @@ public class ZombiePlayerController : MonoBehaviour
     }
     void Start()
     {
-        //GameObject armLeftobj = Instantiate(armPrefab, transform) as GameObject;
-        //GameObject armRightobj = Instantiate(armPrefab, transform) as GameObject;
 
-        //var leftArmComponent = armLeftobj.AddComponent<LimbBehaviour>();
-        // var rightArmComponent = armRightobj.AddComponent<LimbBehaviour>();
-
-        //leftArmComponent.Init(LeftArm);
-        //rightArmComponent.Init(RightArm);
 
         Arm LeftArm = new Arm();
-        Arm RightArm = new Arm();
-
-       
-
+        Arm RightArm = new Arm(); 
 
         m_Inventory.AddLimb(LeftArm);
         m_Inventory.AddLimb(RightArm);
@@ -39,10 +27,7 @@ public class ZombiePlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector2 dir = new Vector3(h, v).normalized;
-        Vector2 pos = transform.position;
-        
-
-        
+        Vector2 pos = transform.position; 
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
