b366ec9d10f6f10b0e753f0a9dccf3686e109265
chuwilliamson <chuwilliamson@gmail.com>
2016-10-21 20:19:48 -0500

    #4
    Player moving

---
+++ ArmAndLeg/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 25; i++)
+            Spawn(prefab);
+    }
+    [SerializeField]
+    public Rect boundary;
+    void Spawn(GameObject go)
+    {
+        float randx = Random.Range(boundary.xMin, boundary.xMax);
+        float randy = Random.Range(boundary.yMin, boundary.yMax);
+        Vector3 randPos = new Vector3(randx, randy, 0);
+        GameObject thing = Instantiate(go, randPos, Quaternion.identity, transform) as GameObject;
+        thing.name = "yeaaaaa";
+
+    }
+
+    // Update is called once per frame
+    public GameObject prefab;
+}
---
+++ ArmAndLeg/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(h, v).normalized;
+        Vector2 pos = transform.position;
+        transform.position = pos + dir * m_Speed * Time.deltaTime;
+    }
+    public float m_Speed;
+    
+}
