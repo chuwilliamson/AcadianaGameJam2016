8d402711568a81cf9a07e16b99d02b6f51c6eca4
Benjamin Odom <bennybroseph@gmail.com>
2016-10-22 00:51:40 -0500

    Limbs Fly Off
    An enemy's limbs are considered different from limb that are on the
    ground, and those that are in the player's inventory since they are game
    objects, but also in someone's inventory. So. They need the component
    EnemyLimbBehaviour added to them. This class inherits from monobehaviour
    (so it is a component) and it hold a reference to a limb object. This
    means that you can give an enemy an already existant limb or create a
    new one and then attach it to your enemy. When the enemy pops a limb off
    of his inventory, that same limb reference is used to create a new game
    object with the component LimbBehaviour at the same position and using
    the same sprite as the limb the zombie had on him.
    
    It also flies off in a random direction with a random rotation. This can
    be set in 'ArmAndLeg/Assets/Settings/Limb Object Factory Settings.asset'
    using the inspector.

--- ArmAndLeg/Assets/Scripts/Items/LimbBehaviour.cs
+++ ArmAndLeg/Assets/Scripts/Items/LimbBehaviour.cs
@@ -9,17 +9,27 @@ namespace Items
         [SerializeField]
         private Limb m_Limb;
 
-        public bool Init(Limb newLimb)
+        [SerializeField]
+        private float m_AliveTime;
+
+        public float aliveTime
+        {
+            get { return m_AliveTime; }
+            set { m_AliveTime = value; }
+        }
+
+        public bool Init(Limb newLimb, float newAliveTime)
         {
             m_Limb = newLimb;
+            m_AliveTime = newAliveTime;
 
             // Attach to the event
             m_Limb.inInventoryEvent.AddListener(
                 inInventory =>
                 {
                     // If no longer in inventory then start to disappear
-                    if (!inInventory)
-                        StartCoroutine(Disappear());
+                    //if (!inInventory)
+                    //    StartCoroutine(Disappear());
                 });
 
             return true;
@@ -27,7 +37,25 @@ namespace Items
 
         private IEnumerator Disappear()
         {
-            yield return new WaitForSeconds(m_Limb.aliveTime);
+            var time = 0f;
+
+            while (time < m_AliveTime)
+            {
+                time += Time.deltaTime;
+
+                var spriteRenderer = GetComponent<SpriteRenderer>();
+                if (spriteRenderer)
+                {
+                    spriteRenderer.color =
+                        new Color(
+                            spriteRenderer.color.r,
+                            spriteRenderer.color.g,
+                            spriteRenderer.color.b,
+                            spriteRenderer.color.a - 1f / m_AliveTime * Time.deltaTime);
+                }
+
+                yield return false;
+            }
 
             if (!m_Limb.inInventory)
                 Destroy(gameObject);
