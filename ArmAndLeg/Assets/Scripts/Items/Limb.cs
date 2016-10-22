d27fcfe04e96fbe7b10511cfa1be5b3fd14abedc
Benjamin Odom <bennybroseph@gmail.com>
2016-10-21 20:02:06 -0500

    Basic Invetory
    Inventory can be interfaced with for UI by adding yourself to the
    'armCountEvent' and 'legCountEvent'. Everytime the value of the arm and
    leg count changes, its new value will be sent as a single integer
    parameter.
    
    Added a basic inventory system
    
    Added a Base Limb class
    
    Added a basic Arm class
    
    Added a basic Leg class

---
+++ ArmAndLeg/Assets/Scripts/Items/Limb.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+namespace Items
+{
+    public abstract class Limb : MonoBehaviour
+    {
+        private bool m_InInventory;
+
+        [SerializeField]
+        private float m_RangedDamage = 1f;
+        [SerializeField]
+        private float m_MeleeDamage = 1f;
+
+        [SerializeField]
+        private float m_AliveTime = 3f;
+
+        public bool inInventory
+        {
+            get { return m_InInventory; }
+            set
+            {
+                m_InInventory = value;
+                if (!value)
+                    StartCoroutine(Update());
+            }
+        }
+
+        public abstract bool Swing();
+        public abstract bool Throw();
+
+        private IEnumerator Update()
+        {
+            yield return new WaitForSeconds(m_AliveTime);
+
+            Destroy(gameObject);
+        }
+    }
+}
