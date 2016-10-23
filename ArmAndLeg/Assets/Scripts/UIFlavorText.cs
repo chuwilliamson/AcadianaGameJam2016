515ad1f115687eed01ebc74fa4093f8a9cc87f5f
chuwilliamson <chuwilliamson@gmail.com>
2016-10-23 03:16:40 -0500

    ui add elements

---
+++ ArmAndLeg/Assets/Scripts/UIFlavorText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIFlavorText : MonoBehaviour
+{
+
+    void Awake()
+    {
+        var inventory = FindObjectOfType<ZombiePlayerController>().inventory;
+        inventory.armCountEvent.AddListener(SayIt);
+        inventory.legCountEvent.AddListener(SayIt);
+    }
+    
+    public List<string> thingsToSay;
+    void SayIt(int num)
+    {
+        int randomindex = Random.Range(0, thingsToSay.Count - 1);
+        if (randomindex < 0)
+            return;
+        string thingToSay = thingsToSay[randomindex];
+        GetComponent<Text>().text = thingToSay;
+    }
+
+
+}
